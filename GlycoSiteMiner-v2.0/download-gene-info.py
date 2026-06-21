25e834a70581997c1c395fdff620bf296aefef17
Neha Rao <raoneha0924@gmail.com>
2026-06-21 17:29:05 -0400

    Refactor gene info download and decompression logic
    Updated the script to use Python's gzip module for decompression and modified the tax ID check to use the correct index.

--- GlycoSiteMiner-v2.0/download-gene-info.py
+++ GlycoSiteMiner-v2.0/download-gene-info.py
@@ -1,6 +1,7 @@
 import sys,os
 import json
 import glob
+import gzip
 import subprocess
 from optparse import OptionParser
 
@@ -21,28 +22,34 @@ def main():
     with open (out_file, "r") as FR:
         for line in FR:
             row = line.strip().split(",")
-            if row[-2] == "yes":
+            # is_reference is 3rd from last; row[-2] is sort_order
+            if row[-3] == "yes":
                 tax_id_dict[row[0].strip()] = True
 
     url = "https://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/All_Data.gene_info.gz"
     
-    out_file = config_obj["data_dir"] + "gene_info/tmp.gene_info.gz"
-    cmd = "curl %s -o %s " % (url, out_file)
-    x = subprocess.getoutput(cmd)
-    cmd = "gunzip " + out_file
+    gz_file = config_obj["data_dir"] + "gene_info/tmp.gene_info.gz"
+    cmd = "curl %s -o %s " % (url, gz_file)
     x = subprocess.getoutput(cmd)
 
-
+    # decompress using Python's gzip module so this works on Windows too
     in_file = config_obj["data_dir"] + "gene_info/tmp.gene_info"
+    with gzip.open(gz_file, "rb") as FZ, open(in_file, "wb") as FD:
+        FD.write(FZ.read())
+    os.remove(gz_file)
     out_file = config_obj["data_dir"] + "gene_info/All_Data.gene_info"
     
-    FW = open(out_file, "w") 
+    FW = open(out_file, "w")
     with open (in_file, "r") as FR:
         for line in FR:
+            # skip comment/header lines
+            if line.startswith("#"):
+                continue
             tax_id = line.split("\t")[0].strip()
             if tax_id in tax_id_dict:
                 FW.write(line)
     FW.close()
+    os.remove(in_file)
 
     cmd = "chmod -R 777 " + config_obj["data_dir"] + "/" + source
     x = subprocess.getoutput(cmd)
